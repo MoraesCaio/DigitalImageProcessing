508e396c8ec09df35cfd713162873613d144148a
Unknown <caiomoraes@msn.com>
2018-03-30 23:42:12 -0300

    Readability on kernel functions

--- dip.py
+++ dip.py
@@ -41,7 +41,7 @@ class ImageMatrix(np.ndarray):
         elif image.format == 'PNG':
             if image.mode != 'RGBA':
                 image = image.convert('RGBA')
-        return np.array(image).view(ImageMatrix)
+        return np.array(image).view(self.__class__)
 
     def get_image(self):
         mode = 'RGBA' if self.shape[2] == 4 else 'RGB'
@@ -49,7 +49,7 @@ class ImageMatrix(np.ndarray):
 
     @staticmethod
     def format_image_array(img_array):
-        return np.uint8(np.clip(img_array, 0, 255)).view(ImageMatrix)
+        return np.uint8(np.clip(img_array, 0, 255)).view(self.__class__)
 
     # Processing methods
     def channel(self, channel_array):
@@ -74,10 +74,7 @@ class ImageMatrix(np.ndarray):
         result[:, :, :3] = 255 - result[:, :, :3]
         return ImageMatrix.format_image_array(result)
 
-    def apply_kernel(self, kernel):
-        kernel = np.flipud(np.fliplr(np.copy(kernel)))
-        result = np.int16(np.copy(self))
-
+    def extension_padded(self, kernel):
         # adding margins
         hks = (kernel.shape[0]-1)/2  # half_kernel_size
         image_padded = np.zeros([self.shape[0] + 2 * hks, self.shape[1] + 2 * hks, self.shape[2]])
@@ -88,66 +85,38 @@ class ImageMatrix(np.ndarray):
         image_padded[:, 0:hks] = image_padded[:, hks:hks+1]
         image_padded[-1: -(hks+1):-1, :] = image_padded[-(hks+1):-(hks+2):-1, :]
         image_padded[:, -1: -(hks+1):-1] = image_padded[:, -(hks+1):-(hks+2):-1]
-        image_padded = np.int16(image_padded)
 
+        return image_padded
+
+    def run_kernel_loop(self, image_padded, kernel):
         # kernel application
         for x in range(self.shape[1]):
             for y in range(self.shape[0]):
                 for c in range(3):
-                    result[y, x, c] = (image_padded[y:y+kernel.shape[0], x:x+kernel.shape[0], c] * kernel).sum()
+                    self[y, x, c] = (image_padded[y:y+kernel.shape[0], x:x+kernel.shape[0], c] * kernel).sum()
 
-        return ImageMatrix.format_image_array(result)
+        return self
+
+    def apply_kernel(self, kernel):
+        kernel = np.flipud(np.fliplr(np.copy(kernel)))
+        output = np.int16(np.copy(self)).view(self.__class__)
+
+        image_padded = np.int16(self.extension_padded(kernel))
+
+        return ImageMatrix.format_image_array(output.run_kernel_loop(image_padded, kernel))
 
     def apply_kernel_float(self, kernel):
         kernel = np.flipud(np.fliplr(np.copy(kernel)))
-        result = np.float64(np.copy(self))
+        output = np.float64(np.copy(self)).view(self.__class__)
 
-        # adding margins
-        hks = (kernel.shape[0]-1)/2  # half_kernel_size
-        image_padded = np.zeros([self.shape[0] + 2 * hks, self.shape[1] + 2 * hks, self.shape[2]])
-        image_padded[hks:-hks, hks:-hks] = self
+        image_padded = np.float64(self.extension_padded(kernel))
 
-        # extension padding on edges
-        image_padded[0:hks, :] = image_padded[hks:hks+1, :]
-        image_padded[:, 0:hks] = image_padded[:, hks:hks+1]
-        image_padded[-1: -(hks+1):-1, :] = image_padded[-(hks+1):-(hks+2):-1, :]
-        image_padded[:, -1: -(hks+1):-1] = image_padded[:, -(hks+1):-(hks+2):-1]
-        image_padded = np.float64(image_padded)
-
-        # kernel application
-        for x in range(self.shape[1]):
-            for y in range(self.shape[0]):
-                for c in range(3):
-                    result[y, x, c] = (image_padded[y:y+kernel.shape[0], x:x+kernel.shape[0], c] * kernel).sum()
-
-        return ImageMatrix.format_image_array(result)
-
-    def to_yiq(self):
-        t = np.array([[0.299, 0.587, 0.114], [0.596, -0.274, -0.322], [0.211, -0.523, 0.312]])
-        fl_copy = np.float64(np.copy(self[:, :, :3]))
-        result = fl_copy.dot(t.T) / 255
-        return result.view(ImageMatrix)
-
-    def to_rgb(self):
-        t = np.array([[1.000, 0.956, 0.621], [1.000, -0.272, -0.647], [1.000, -1.106, 1.703]])
-        fl_copy = np.float64(np.copy(self[:, :, :3]))
-        result = fl_copy.dot(t.T) * 255.0
-        return ImageMatrix.format_image_array(result)
+        return ImageMatrix.format_image_array(output.run_kernel_loop(image_padded, kernel))
 
     def median(self, kernel):
-        result = np.int16(np.copy(self))
+        output = np.int16(np.copy(self))
 
-        # adding margins
-        hks = (kernel.shape[0]-1)/2  # half_kernel_size
-        image_padded = np.zeros([self.shape[0] + 2 * hks, self.shape[1] + 2 * hks, self.shape[2]])
-        image_padded[hks:-hks, hks:-hks] = self
-
-        # extension padding on edges
-        image_padded[0:hks, :] = image_padded[hks:hks+1, :]
-        image_padded[:, 0:hks] = image_padded[:, hks:hks+1]
-        image_padded[-1: -(hks+1):-1, :] = image_padded[-(hks+1):-(hks+2):-1, :]
-        image_padded[:, -1: -(hks+1):-1] = image_padded[:, -(hks+1):-(hks+2):-1]
-        image_padded = np.int16(image_padded)
+        image_padded = np.int16(self.extension_padded(kernel))
 
         # median application
         for x in range(self.shape[1]):
@@ -155,10 +124,22 @@ class ImageMatrix(np.ndarray):
                 for c in range(3):
                     copy1d = image_padded[y:y+kernel.shape[0], x:x+kernel.shape[0], c].flatten()
                     copy1d.sort()
-                    result[y, x, c] = copy1d[hks+1]
+                    output[y, x, c] = copy1d[hks+1]
                     # does not work! kernel must not be applied over a backup array
-                    # image_padded[y+hks, x+hks, c] = copy1d[hks+1] 
+                    # image_padded[y+hks, x+hks, c] = copy1d[hks+1]
 
+        return ImageMatrix.format_image_array(output)
+
+    def to_yiq(self):
+        t = np.array([[0.299, 0.587, 0.114], [0.596, -0.274, -0.322], [0.211, -0.523, 0.312]])
+        fl_copy = np.float64(np.copy(self[:, :, :3]))
+        result = fl_copy.dot(t.T) / 255
+        return result.view(self.__class__)
+
+    def to_rgb(self):
+        t = np.array([[1.000, 0.956, 0.621], [1.000, -0.272, -0.647], [1.000, -1.106, 1.703]])
+        fl_copy = np.float64(np.copy(self[:, :, :3]))
+        result = fl_copy.dot(t.T) * 255.0
         return ImageMatrix.format_image_array(result)
 
 # def set_y_luma(img, x, y):
