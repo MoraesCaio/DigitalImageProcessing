efd48e9df16beb5915fe2ac919e6153306e8bf88
samueldemoura <samueldemouramoreira@gmail.com>
2018-04-24 22:38:47 -0300

    Implement command line options

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import dip
 from dip import Filter
 from dip import ImageMatrix
@@ -5,16 +6,6 @@ from dip import Routine
 import numpy as np
 
 
-filename = ['test.jpeg', 'test2.png', '1px.png', 'petala.png', 'SnPnoise.png', 'valve.png', 'bike.jpg',
-            'xray.jpeg', 'xray2.jpeg']
-
-# petala = ImageMatrix.from_file(filename[3])
-# noise = ImageMatrix.from_file(filename[4])
-# valve = ImageMatrix.from_file(filename[5])
-# bike = ImageMatrix.from_file(filename[6])
-# xray1 = ImageMatrix.from_file(filename[7])
-# xray2 = ImageMatrix.from_file(filename[8])
-
-petala = Routine("petala.png")
-petala.threshold_y()
-petala.threshold_mean_y()
+img = Routine(sys.argv[1])
+expression = 'img.{0}({1})'.format(sys.argv[2], ', '.join(sys.argv[3:]))
+eval(expression)
